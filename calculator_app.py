d2dacdae25ac67a319c080da0d0512822a07dda4
leslieannumali031788 <umalileslieann10@gmail.com>
2026-05-06 00:33:27 +0800

    Added print statement for multiplication and division.

--- calculator_app.py
+++ calculator_app.py
@@ -26,3 +26,7 @@ def main():
     calculator = OtherOperations()
 
     print("\n===== Calculator =====")
+    print("1 - Addition")
+    print("2 - Subtraction")
+    print("3 - Multiplication")
+    print("4 - Division")
